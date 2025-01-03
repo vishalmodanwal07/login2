66484b442e93adc75b1abfa469a1f9d713c19208
vishal modanwal <vk0764394@gmail.com>
2025-01-03 13:51:09 +0530

    frontend setup

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
+
+// Load environment variables from the `.env` file
+dotenv.config();
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": process.env.VITE_API_BASE_URL,
+    },
+  },
   plugins: [react()],
-})
+});
